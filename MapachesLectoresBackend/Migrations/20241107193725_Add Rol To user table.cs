064878515679ead792ebcb9c4850bcf7e7c83973
Pedro <pedro.pelayo003@gmail.com>
2024-11-07 20:50:20 +0100

    Agregadas las migraciones para añadir campo de rol en la tabla usuario

---
+++ MapachesLectoresBackend/Migrations/20241107193725_Add Rol To user table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MapachesLectoresBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRolTousertable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "Role",
+                table: "user",
+                type: "int unsigned",
+                nullable: false,
+                defaultValue: 0u);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "user");
+        }
+    }
+}
