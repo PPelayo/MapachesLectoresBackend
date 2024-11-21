24d0571bcc9386c396db5f627e5dc3cd212c1380
Pedro Manuel Martinez Pelayo <pedromanuel.martinez@viewnext.com>
2024-11-21 18:24:04 +0100

    feat: Agregados datos de prueba a la tabla category

--- MapachesLectoresBackend/Books/Data/Db/CategoryModelCreating.cs
+++ MapachesLectoresBackend/Books/Data/Db/CategoryModelCreating.cs
@@ -13,6 +13,30 @@ public class CategoryModelCreating
         {
             entity.ToTable("category");
             entity.HasKey(e => e.Id);
+
+            entity.HasData(DefaultData());
         });
+    }
+
+    private static IEnumerable<Category> DefaultData()
+    {
+        return
+        [
+            new Category { Id = 1, Description = "Terror", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174000" },
+            new Category { Id = 2, Description = "Romance", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174001" },
+            new Category { Id = 3, Description = "Suspense", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174002" },
+            new Category { Id = 4, Description = "Policiaca", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174003" },
+            new Category { Id = 5, Description = "Ciencia Ficción", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174004" },
+            new Category { Id = 6, Description = "Fantasía", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174005" },
+            new Category { Id = 7, Description = "Aventuras", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174006" },
+            new Category { Id = 8, Description = "Misterio", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174007" },
+            new Category { Id = 9, Description = "Drama", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174008" },
+            new Category { Id = 10, Description = "Comedia", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174009" },
+            new Category { Id = 11, Description = "Historia", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174010" },
+            new Category { Id = 12, Description = "Biografía", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174011" },
+            new Category { Id = 13, Description = "Autoayuda", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174012" },
+            new Category { Id = 14, Description = "Cocina", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174013" },
+            new Category { Id = 15, Description = "Viajes", CreatedAt = DateTime.Parse("2024-01-01"), UpdatedAt = DateTime.Parse("2024-01-01"), ItemUuid = "123e4567-e89b-12d3-a456-426614174014" },
+        ];
     }
 }
---
+++ MapachesLectoresBackend/Migrations/20241121171515_AddedDefaultDataToCategories.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MapachesLectoresBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultDataToCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "category",
+                columns: new[] { "Id", "CreatedAt", "Description", "ItemUuid", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Terror", "123e4567-e89b-12d3-a456-426614174000", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Romance", "123e4567-e89b-12d3-a456-426614174001", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Suspense", "123e4567-e89b-12d3-a456-426614174002", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Policiaca", "123e4567-e89b-12d3-a456-426614174003", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ciencia Ficción", "123e4567-e89b-12d3-a456-426614174004", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 6u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fantasía", "123e4567-e89b-12d3-a456-426614174005", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 7u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Aventuras", "123e4567-e89b-12d3-a456-426614174006", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 8u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Misterio", "123e4567-e89b-12d3-a456-426614174007", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 9u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Drama", "123e4567-e89b-12d3-a456-426614174008", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 10u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Comedia", "123e4567-e89b-12d3-a456-426614174009", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 11u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Historia", "123e4567-e89b-12d3-a456-426614174010", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 12u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Biografía", "123e4567-e89b-12d3-a456-426614174011", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 13u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Autoayuda", "123e4567-e89b-12d3-a456-426614174012", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 14u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Cocina", "123e4567-e89b-12d3-a456-426614174013", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 15u, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Viajes", "123e4567-e89b-12d3-a456-426614174014", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 1u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 2u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 3u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 4u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 5u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 6u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 7u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 8u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 9u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 10u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 11u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 12u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 13u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 14u);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 15u);
+        }
+    }
+}
