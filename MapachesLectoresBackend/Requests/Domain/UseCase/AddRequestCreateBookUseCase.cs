0d7e23cd0dc60a823a290cc92a457189b717ad27
Pedro Manuel Martinez Pelayo <pedromanuel.martinez@viewnext.com>
2024-12-04 17:56:30 +0100

    feat: Agregado endpoint para insertar request de create book

---
+++ MapachesLectoresBackend/Requests/Domain/UseCase/AddRequestCreateBookUseCase.cs
@@ -0,0 +1,38 @@
+﻿using MapachesLectoresBackend.Books.Domain.Model;
+using MapachesLectoresBackend.Books.Domain.Model.Dto;
+using MapachesLectoresBackend.Core.Domain.Model.Errors;
+using MapachesLectoresBackend.Core.Domain.Model.ResultPattern;
+using MapachesLectoresBackend.Core.Domain.Repository;
+using MapachesLectoresBackend.Core.Domain.UseCase;
+using MapachesLectoresBackend.Requests.Domain.Model;
+
+namespace MapachesLectoresBackend.Requests.Domain.UseCase
+{
+    public class AddRequestCreateBookUseCase(
+        IRepository<RequestCreateBook> repository,
+        IRepository<Publisher> publisherRepository     
+    )
+    {
+        public async Task<DataResult<RequestCreateBook>> InvokeAsync(CreateBookRequestDto createBookRequestDto)
+        {
+            var publisher = await publisherRepository.GetByUuidAsync(createBookRequestDto.PublisherId.ToString());
+
+            if (publisher == null)
+                return DataResult<RequestCreateBook>.CreateFailure(new NotFoundError());
+
+            var request = new RequestCreateBook()
+            {
+                Name = createBookRequestDto.Name,
+                Synopsis = createBookRequestDto.Synopsis,
+                NumberOfPages = createBookRequestDto.NumberOfPages,
+                PublishedDate = createBookRequestDto.PublishedDate,
+                PublisherId = publisher.Id,
+                CoverUrl = ""
+            };
+
+            var insertedRequest = await repository.InsertAsync(request);
+
+            return DataResult<RequestCreateBook>.CreateSuccess(insertedRequest);
+        }
+    }
+}
--- MapachesLectoresBackend/Requests/Presentation/Controller/RequestBookController.cs
+++ MapachesLectoresBackend/Requests/Presentation/Controller/RequestBookController.cs
@@ -1,4 +1,5 @@
-﻿using MapachesLectoresBackend.Core.Domain.Model.Pagination;
+﻿using MapachesLectoresBackend.Books.Domain.Model.Dto;
+using MapachesLectoresBackend.Core.Domain.Model.Pagination;
 using MapachesLectoresBackend.Requests.Domain.UseCase;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,17 +7,28 @@ namespace MapachesLectoresBackend.Requests.Presentation.Controller
 {
     [ApiController]
     public class RequestBookController(
-        GetRequestsCreateBookUseCase getRequestsCreateBookUseCase    
+        GetRequestsCreateBookUseCase getRequestsCreateBookUseCase,
+        AddRequestCreateBookUseCase addRequestCreateBookUseCase
         
     ) : ControllerBase
     {
 
-        [HttpGet("/create")]
+        [HttpGet("create")]
         public async Task<IActionResult> GetCreateRequest(
              [FromQuery] UserPagination pagination    
         )
         {
             var result = getRequestsCreateBookUseCase.InvokeAsync(pagination);
+
+            return Ok(result);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> InsertCreateRequest(
+            [FromBody] CreateBookRequestDto createBookRequestDto    
+        )
+        {
+            var result = await addRequestCreateBookUseCase.InvokeAsync(createBookRequestDto);
 
             return Ok(result);
         }
